35f198e6c80004f129861e393c74f9bf706d6f55
Tim Kellogg <tim.kellogg@hyfn.com>
2017-04-15 17:00:00 -0700

    add vue-cli client build

--- controllers/pages.go
+++ controllers/pages.go
@@ -5,10 +5,10 @@ import (
 	"net/http"
 )
 
-// PagesHome - "/" index route handler for application
+// PagesHome - "/" index route handler for index
 func PagesHome(w http.ResponseWriter, h *http.Request) {
 	d := PageData{Title: "531 Trainer"}
 
-	t, _ := template.ParseFiles("views/application.html")
+	t, _ := template.ParseFiles("views/index.html")
 	t.Execute(w, d)
 }
