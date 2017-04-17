3d2bbcf0b2dfa21f46f7e925d8bef2b65b32319d
Tim Kellogg <tim.kellogg@hyfn.com>
2017-04-16 17:06:23 -0700

    add basic frontend

--- views/src/main.js
+++ views/src/main.js
@@ -2,14 +2,19 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import store from './state'
 import router from './router'
 
+Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   template: '<App/>',
   components: { App }
 })
